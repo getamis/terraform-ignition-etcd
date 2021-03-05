00a076f1703b72d69d3676c16b7ee8b4e2080518
ChihChieh Huang <chihchieh_huang@maicoin.com>
2021-03-05 16:28:52 +0800

    Fix disk and filesystem, make the partition and filesystem reusable
    Beware about:
    - the `Filesystem-Reuse Semantics`
        - https://github.com/coreos/ignition/blob/v2.1.0/doc/operator-notes.md#filesystem-reuse-semantics
    - `Partition Reuse Semantics`
        - https://github.com/coreos/ignition/blob/v2.1.0/doc/operator-notes.md#partition-reuse-semantics
    
    We should assign both label and UUID for filesystem, PARTLABEL for partition.
    Also, use nvmen*p1 instead of nvmen* to avoid filesystem clean up the PARTLABEL.

--- volume.tf
+++ volume.tf
@@ -1,16 +1,25 @@
+resource "random_uuid" "etcd_data_fs_uuid" {
+}
+
 data "ignition_filesystem" "ectd_data" {
   name = "etcd-data"
 
   mount {
-    device = var.device_name
-    format = "ext4"
+    device          = local.device_partition_name
+    format          = "ext4"
+    wipe_filesystem = false
+    label           = "etcd-data"
+    uuid            = random_uuid.etcd_data_fs_uuid.result
   }
 }
 
 data "ignition_disk" "ectd_data" {
   device = var.device_name
 
+  wipe_table = false
+
   partition {
+    label  = "ETCD-DATA"
     number = 1
     start  = 0
     size   = 0
@@ -19,13 +28,14 @@ data "ignition_disk" "ectd_data" {
 
 locals {
   systemd_etcd_data_mount_name = replace(trimprefix(var.data_path, "/"), "/", "-")
+  device_partition_name = "${var.device_name}p1"
 }
 
 data "ignition_systemd_unit" "etcd_data_mount" {
   name = "${local.systemd_etcd_data_mount_name}.mount"
 
   content = templatefile("${path.module}/templates/data.mount.tpl", {
-    device_name = var.device_name
+    device_name = local.device_partition_name
     data_path   = var.data_path
   })
 }
